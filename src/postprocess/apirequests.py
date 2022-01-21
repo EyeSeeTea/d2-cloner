92e3b0eb4f65ee25d547d9b88a556a56371ea7d0
root <root@idelcano-OMEN-by-HP-Desktop-PC-880-p0xx>
2022-01-21 12:52:48 +0100

    increase the total timeout due orgunit remove take long time

--- src/postprocess/apirequests.py
+++ src/postprocess/apirequests.py
@@ -14,7 +14,7 @@ def init_api(url, username, password):
     return dhis2api.Dhis2Api(url, username, password)
 
 
-def wait_for_server(api, timeout=1800):
+def wait_for_server(api, timeout=43200):
     "Sleep until server is ready to accept requests"
     debug("Check active API: %s" % api.api_url)
     import time as time_
@@ -24,10 +24,12 @@ def wait_for_server(api, timeout=1800):
             api.get("/me")
             break
         except requests.exceptions.HTTPError:
+            debug("(HttpError) The instance is booting, it may take a few minutes...  Please wait")
             if time_.time() - start_time > timeout:
                 raise RuntimeError("Timeout: could not connect to the API")
             time_.sleep(120)
         except requests.exceptions.ConnectionError:
+            debug("(Connection) The instance is booting, it may take a few minutes... Please wait")
             if time_.time() - start_time > timeout:
                 raise RuntimeError("Timeout: could not connect to the API")
             time_.sleep(120)
