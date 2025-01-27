ab3ce24ed232037c6f241b8b22dfe93c6ed2c81b
idelcano <ignaciodelcano@gmail.com>
2025-01-27 19:05:32 +0100

    Added enum with the table options

---
+++ src/preprocess/sql_generation/table_key.py
@@ -0,0 +1,9 @@
+
+from enum import Enum
+
+class TableKey(Enum):
+    EVENT = "event"
+    ENROLLMENT = "enrollment"
+    TRACKER = "tracker"
+    EVENT_COMMENT = "event_comment"
+    ENROLLMENT_COMMENT = "enrollment_comment"
