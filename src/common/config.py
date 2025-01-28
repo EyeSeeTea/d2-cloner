ff1b19470426e0045e92ab8791932634d944a4f2
idelcano <ignaciodelcano@gmail.com>
2025-01-28 08:06:02 +0100

    Added methods to call directly to the getters, and convert the given string to number to allow more operations

--- src/common/config.py
+++ src/common/config.py
@@ -5,12 +5,28 @@ class Config:
     def __new__(cls, pre_api_version=None, post_api_version=None):
         if cls._instance is None:
             cls._instance = super(Config, cls).__new__(cls)
-            cls._instance.pre_api_version = pre_api_version
-            cls._instance.post_api_version = post_api_version
+            cls._instance.pre_api_version = cls.convert_to_number(pre_api_version)
+            cls._instance.post_api_version = cls.convert_to_number(post_api_version)
         return cls._instance
+
+    @staticmethod
+    def convert_to_number(version):
+        """Convert the given string into a number, removing the '2.' prefix if present in the config file."""
+        if isinstance(version, str):
+            version = version.replace("2.", "")
+            return int(version)
+        return version
 
     @classmethod
     def get_instance(cls):
         if cls._instance is None:
             raise ValueError("Config has not been initialized. Call Config(...) first.")
         return cls._instance
+
+    @classmethod
+    def get_pre_api_version(cls):
+        return cls.get_instance().pre_api_version
+
+    @classmethod
+    def get_post_api_version(cls):
+        return cls.get_instance().post_api_version
