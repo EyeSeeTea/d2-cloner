22801c2736187dbbbe231de7fd91f827f916fc52
idelcano <ignaciodelcano@gmail.com>
2025-08-29 20:28:18 +0200

    update datasets to remove also unlisted uids

--- src/preprocess/sql_generation/queries/delete_datasets.py
+++ src/preprocess/sql_generation/queries/delete_datasets.py
@@ -3,9 +3,12 @@ from src.preprocess.sql_generation.sql_common import convert_to_sql_format, writ
 
 def generate_delete_datasets_rules(datasets, data_elements, org_units,
                                    org_unit_descendants, all_uid, f):
-    write(f, "--remove datasets" + "\n")
     sql_all = convert_to_sql_format(all_uid)
     sql_datasets = convert_to_sql_format(datasets)
+    write(f, f"""
+    SELECT 'Starting DELETE block  for dataSets: All: ' || quote_literal($${sql_all or "NO_IDS"}$$) ||
+           ' and Detailed: ' || quote_literal($${sql_datasets or "NO_IDS"}$$) AS info;
+    """)
     sql_data_elements = convert_to_sql_format(data_elements)
     sql_org_units = convert_to_sql_format(org_units)
     sql_org_unit_descendants = convert_to_sql_format(org_unit_descendants)
@@ -51,6 +54,10 @@ def generate_delete_datasets_rules(datasets, data_elements, org_units,
     else:
         write(f, fix_final_query(sql_query) + "\n")
 
+    write(f, f"""
+    SELECT 'Close DELETE Dataset Block' AS info;
+    """)
+
 
 def delete_all_data_sets_from_lists(programs, f):
     programs = convert_to_sql_format(programs)
@@ -63,14 +70,16 @@ def delete_all_data_sets_not_in_lists(programs, f):
 
 
 def delete_all_data_sets(datasets, f, exclude=False):
+    write(f, f"""
+    SELECT 'Starting DELETE block  for dataSets: All: ' || quote_literal($${datasets or "NO_IDS"}$$) AS info;
+    """)
     operator = "not in" if exclude else "in"
     write(f, """
-        --remove all datasets
-        DELETE FROM datavalueaudit where dataelementid in
+        DELETE FROM datavalueaudit where dataelementid {operator}
         (select dataelementid from datasetelement
-        where datasetid in (select datasetid from dataset where uid {operator} {datasets}));
+        where datasetid in (select datasetid from dataset where uid in {datasets}));
     """.format(datasets=datasets, operator=operator))
     write(f, """
-        DELETE FROM datavalue where dataelementid in (select dataelementid from datasetelement
-        where datasetid in (select datasetid from dataset where uid {operator} {datasets}));
+        DELETE FROM datavalue where dataelementid {operator} (select dataelementid from datasetelement
+        where datasetid in (select datasetid from dataset where uid in {datasets}));
     """.format(datasets=datasets, operator=operator))
