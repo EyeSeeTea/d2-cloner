eb3c0f747f06b6999ccc0251da2364e81edf9b4c
idelcano <ignaciodelcano@gmail.com>
2025-10-24 09:53:00 +0200

    Refactor selects and add common sufix

--- src/preprocess/sql_generation/queries/delete_trackers.py
+++ src/preprocess/sql_generation/queries/delete_trackers.py
@@ -11,7 +11,7 @@ def generate_delete_tracker_rules(trackers, data_elements, org_units, org_unit_d
     tracker_program_uids_sql = sql_trackers if sql_trackers != "" else sql_all
     if tracker_program_uids_sql == "":
         write(f, f"""
-        SELECT 'No UIDs provided for this department; skipping tracked entity and tracker program deletion' AS info;
+        SELECT 'No UIDs provided for this department; skipping tracked entity and tracker program deletion' AS D2_DOCKER_PRESQL_SCRIPT;
         """)
         return
 
@@ -20,13 +20,22 @@ def generate_delete_tracker_rules(trackers, data_elements, org_units, org_unit_d
     SELECT 'Starting DELETE block for trackerPrograms: All: '
            || quote_literal($${sql_all or 'NO_IDS'}$$)
            || ' and Detailed: '
-           || quote_literal($${sql_trackers or 'NO_IDS'}$$) as info;
+           || quote_literal($${sql_trackers or 'NO_IDS'}$$) as D2_DOCKER_PRESQL_SCRIPT;
     """)
     sql_data_elements = convert_to_sql_format(data_elements)
     sql_org_units = convert_to_sql_format(org_units)
     sql_org_unit_descendants = convert_to_sql_format(org_unit_descendants)
 
     if sql_data_elements != "" or sql_org_units != "" or sql_org_unit_descendants != "":
+        write(f, f"""
+            SELECT
+                'Starting CUSTOM QUERY block for eventPrograms: '
+                || quote_literal($${tracker_program_uids_sql or 'NO_IDS'}$$)
+                || ' | dataelements: ' || quote_literal($${sql_data_elements or 'NO_IDS'}$$)
+                || ' | org_units: ' || quote_literal($${sql_org_units or 'NO_IDS'}$$)
+                || ' | org_unit_descendants: ' || quote_literal($${sql_org_unit_descendants or 'NO_IDS'}$$)
+            AS "D2_DOCKER_PRESQL_SCRIPT";
+        """)
         sql_query = compose_custom_query(tracker_program_uids_sql, sql_data_elements, sql_org_units, sql_org_unit_descendants)
         delete_mandatory_dependencies(f, tracker_program_uids_sql)
         write(f, fix_final_query(sql_query) + "\n")
@@ -38,18 +47,18 @@ def compose_custom_query(tracker_uis, sql_data_elements, sql_org_units, sql_org_
     if sql_data_elements != "":
         # Removing these data elements requires updating the eventdatavalues JSONB.
         sql_query = """ 
-                     update {programstageinstance} set eventdatavalues = eventdatavalues - {remove_data_elements_uid}  where eventdatavalues ? 
+                     update {event} set eventdatavalues = eventdatavalues - {remove_data_elements_uid}  where eventdatavalues ? 
                      {where_data_elements_uid} and programstageid in (select programstageid from programstage where programid in
                      (select programid from program where uid in {tracker_program_uid})) and 
-                 """.format(programstageinstance=get_event_table_name(), remove_data_elements_uid=sql_data_elements,
+                 """.format(event=get_event_table_name(), remove_data_elements_uid=sql_data_elements,
                             where_data_elements_uid=sql_data_elements, tracker_program_uid=tracker_uis)
     else:
         # If we don't need to remove data values, we should delete the events.
         sql_query = """
-            DELETE FROM {programstageinstance} where programstageid in 
+            DELETE FROM {event} where programstageid in 
             (select programstageid from programstage where programid in 
             (select programid from program where uid in {uids})) 
-        """.format(programstageinstance=get_event_table_name(), uids=tracker_uis)
+        """.format(event=get_event_table_name(), uids=tracker_uis)
 
     if sql_org_units != "":
         sql_query = sql_query + """ 
@@ -70,26 +79,28 @@ def compose_custom_query(tracker_uis, sql_data_elements, sql_org_units, sql_org_
 def delete_mandatory_dependencies(f, trackers, exclude=False):
     """this query is called only when we are trying to remove only a custom dataelements/orgunits/orgunit leveles from tracker programs"""
     operator = "not in" if exclude else "in"
+
+    write(f, "SELECT 'Deleting All mandatory dependencies for custom deletion....' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
         --remove all tracker dependencies
-        DELETE FROM trackedentitydatavalueaudit where {programstageinstanceid}
-        in ( select psi.{programstageinstanceid}  from {programstageinstance} psi
-        inner join programstage ps on ps.programstageid=psi.programstageid
+        DELETE FROM trackedentitydatavalueaudit where {eventid}
+        in ( select e.{eventid}  from {event} e
+        inner join programstage ps on ps.programstageid=e.programstageid
         inner join program p on p.programid=ps.programid
         where p.uid {operator} {tracker_uids});
-    """.format(programstageinstanceid=get_event_identifier_name(), programstageinstance=get_event_table_name(),
+    """.format(eventid=get_event_identifier_name(), event=get_event_table_name(),
                tracker_uids=trackers, operator=operator))
     write(f, """
-        DELETE FROM {programstageinstancecomments} where {programstageinstanceid}
-        in ( select {programstageinstanceid} from {programstageinstance} where programstageid in
+        DELETE FROM {event_comment} where {eventid}
+        in ( select {eventid} from {event} where programstageid in
         (select programstageid from programstage where programid in
         (select programid from program where uid {operator} {tracker_uids}))); 
-    """.format(programstageinstancecomments=get_event_comment_table(),
-               programstageinstanceid=get_event_identifier_name(), programstageinstance=get_event_table_name(),
+    """.format(event_comment=get_event_comment_table(),
+               eventid=get_event_identifier_name(), event=get_event_table_name(),
                tracker_uids=trackers, operator=operator))
 
     write(f, f"""
-    SELECT 'Close DELETE TRACKER PROGRAM Block' AS info;
+    SELECT 'Close DELETE TRACKER PROGRAM Block' AS D2_DOCKER_PRESQL_SCRIPT;
     """)
 
 def delete_all_tracker_programs_from_lists(trackers, f):
@@ -104,7 +115,7 @@ def delete_all_tracker_programs_not_in_lists(trackers, f):
 def create_index_to_improve_deletion(f):
     write(f, """
         CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_tei_to_remove_trid ON tei_to_remove({trackedentityid}); 
-         CREATE INDEX IF NOT EXISTS idx_events_to_remove ON events_to_remove ({eventid});
+        CREATE INDEX IF NOT EXISTS idx_events_to_remove ON events_to_remove ({eventid});
         CREATE INDEX IF NOT EXISTS idx_enrollments_to_remove ON enrollments_to_remove ({enrollmentid});
         CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_programs_to_remove_pid ON programs_to_remove(programid);
     """.format(trackedentityid=get_tracker_identifier_name(), event=get_event_table_name(),
@@ -115,13 +126,13 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
     write(f, f"""
         SELECT 'Starting DELETE block for trackerPrograms: '
                || quote_literal($${trackers or 'NO_IDS'}$$)
-               || ' not in?: {exclude}' AS info;
+               || ' not in?: {exclude}' AS D2_DOCKER_PRESQL_SCRIPT;
         """)
     operator = "not in" if exclude else "in"
 
     if not trackers:
         #If the selected or unlisted tracker uids is empty, we must exit without remove trackers
-        write(f, "SELECT 'No tracker UIDs provided; skipping delete block' AS info;\n")
+        write(f, "SELECT 'No tracker UIDs provided; skipping delete block' AS D2_DOCKER_PRESQL_SCRIPT;\n")
         return
 
     #Careful: since the NOT IN operator is applied in the following queries, the initial one must be an IN for all cases.
@@ -175,13 +186,13 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
     create_index_to_improve_deletion(f)
     #Informative query
     write(f, """
-      SELECT 'Events to be deleted: ' || COUNT(*)::text AS info FROM events_to_remove;
-      SELECT 'Enrollments to be deleted: ' || COUNT(*)::text FROM enrollments_to_remove;
-      SELECT 'Tracked entities to be deleted: ' || COUNT(*)::text FROM tei_to_remove;
+      SELECT 'Events to be deleted: ' || COUNT(*)::text AS D2_DOCKER_PRESQL_SCRIPT FROM events_to_remove;
+      SELECT 'Enrollments to be deleted: ' || COUNT(*)::text as D2_DOCKER_PRESQL_SCRIPT FROM enrollments_to_remove;
+      SELECT 'Tracked entities to be deleted: ' || COUNT(*)::text as D2_DOCKER_PRESQL_SCRIPT FROM tei_to_remove;
     """)
     # Remove Tracked entity data value audits (audits from events) -  program and tei-enrollment-program
 
-    write(f, "SELECT 'Deleting trackedentitydatavalueaudit....' AS info;\n")
+    write(f, "SELECT 'Deleting trackedentitydatavalueaudit....' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
         DELETE FROM trackedentitydatavalueaudit 
         WHERE {eventid} IN (SELECT {eventid} FROM events_to_remove);
@@ -194,7 +205,7 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
     ))
 
     # Remove Event comments in program and tei-enrollment-program
-    write(f, "SELECT 'Deleting event_comment....' AS info;\n")
+    write(f, "SELECT 'Deleting event_comment....' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
         DELETE FROM {event_comment} 
         WHERE {eventid} IN (SELECT {eventid} FROM events_to_remove);
@@ -208,7 +219,7 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
     ))
 
     # Remove TrackedAttributeValueAudits in teis from programs
-    write(f, "SELECT 'Deleting trackedentityattributevalueaudit....' AS info;\n")
+    write(f, "SELECT 'Deleting trackedentityattributevalueaudit....' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
         DELETE FROM trackedentityattributevalueaudit
         WHERE {trackedentityid} IN (SELECT trackedentityid FROM tei_to_remove);
@@ -217,7 +228,7 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
         enrollment=get_enrollment_table_name()
     ))
     # TrackedAttributeValues in teis from programs
-    write(f, "SELECT 'Deleting trackedentityattributevalue....' AS info;\n")
+    write(f, "SELECT 'Deleting trackedentityattributevalue....' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
         DELETE FROM trackedentityattributevalue
         WHERE {trackedentityid} IN (SELECT trackedentityid FROM tei_to_remove);
@@ -227,7 +238,7 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
     ))
 
     # Delete events from the selected programs and any events linked via enrollments of tracked entities marked for removal (even if they belong to other programs)
-    write(f, "SELECT 'Deleting event....' AS info;\n")
+    write(f, "SELECT 'Deleting event....' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
         DELETE FROM {event} e
         WHERE {eventid} IN (SELECT {eventid} FROM events_to_remove);
@@ -236,7 +247,7 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
     ))
 
     # Remove enrollment_comment in programs and teis.
-    write(f, "SELECT 'Deleting enrollment_comment....' AS info;\n")
+    write(f, "SELECT 'Deleting enrollment_comment....' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
         DELETE FROM {enrollment_comment}
         WHERE {enrollmentid} IN (select {enrollmentid} FROM enrollments_to_remove);
@@ -246,7 +257,7 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
     ))
 
     # Remove enrollment in programs and teis.
-    write(f, "SELECT 'Deleting enrollment....' AS info;\n")
+    write(f, "SELECT 'Deleting enrollment....' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
         DELETE FROM {enrollment} e
         WHERE {enrollmentid} IN (select {enrollmentid} FROM enrollments_to_remove);
@@ -256,7 +267,7 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
     ))
 
     # 9) Limpieza final de TEIs y relaciones
-    write(f, "SELECT 'Deleting trackedentity and dependencies....' AS info;\n")
+    write(f, "SELECT 'Deleting trackedentity and dependencies....' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
         DELETE FROM trackedentityattributevalue WHERE {trackedentityid} IN (SELECT trackedentityid FROM tei_to_remove);
         DELETE FROM trackedentityprogramowner WHERE {trackedentityid} IN (SELECT trackedentityid FROM tei_to_remove);
@@ -272,4 +283,4 @@ def delete_all_tracker_programs(trackers, f, exclude=False):
         trackedentityid=get_tracker_identifier_name()
     ))
 
-    write(f, "SELECT 'Close DELETE TRACKER PROGRAM Block' AS info;\n")
+    write(f, "SELECT 'Close DELETE TRACKER PROGRAM Block' AS D2_DOCKER_PRESQL_SCRIPT;\n")
