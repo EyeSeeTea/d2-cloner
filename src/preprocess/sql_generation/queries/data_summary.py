e87b583661a8aabb88fbcba3b0e650eabbf73b8c
idelcano <ignaciodelcano@gmail.com>
2025-10-24 11:11:40 +0200

    Add identifier and fix typo

--- src/preprocess/sql_generation/queries/data_summary.py
+++ src/preprocess/sql_generation/queries/data_summary.py
@@ -3,14 +3,13 @@ from src.preprocess.sql_generation.sql_common import write
 
 def show_data_summary(f):
     count_datavalues_grouped_per_dataset(f)
-    count_events_gropued_per_program(f)
+    count_events_grouped_per_program(f)
     count_enrollments_grouped_per_program(f)
     count_tracked_entities_grouped_per_program(f)
 
-
 def count_datavalues_grouped_per_dataset(f):
     write(f, "\n-- Data values per data set\n")
-    write(f, "SELECT 'Database summary - data values per data set' AS info;\n")
+    write(f, "SELECT 'Database summary - data values per data set' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
 SELECT
     ds.uid   AS dataset_uid,
@@ -24,10 +23,9 @@ GROUP BY ds.uid, ds.name
 ORDER BY ds.name DESC;
 """)
 
-
-def count_events_gropued_per_program(f):
+def count_events_grouped_per_program(f):
     write(f, "\n-- Events per program\n")
-    write(f, "SELECT 'Database summary - events per program' AS info;\n")
+    write(f, "SELECT 'Database summary - events per program' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
 SELECT
     p.uid AS program_uid,
@@ -40,10 +38,9 @@ GROUP BY p.uid, p.name
 ORDER BY p.name DESC;
 """)
 
-
 def count_enrollments_grouped_per_program(f):
     write(f, "\n-- Enrollments per program\n")
-    write(f, "SELECT 'Database summary - enrollments per program' AS info;\n")
+    write(f, "SELECT 'Database summary - enrollments per program' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
 SELECT
     p.uid  AS program_uid,
@@ -55,10 +52,9 @@ GROUP BY p.uid, p.name
 ORDER BY p.name DESC;
 """)
 
-
 def count_tracked_entities_grouped_per_program(f):
     write(f, "\n-- Tracked entity instances (TEIs) per program\n")
-    write(f, "SELECT 'Database summary - tracked entity instances per program' AS info;\n")
+    write(f, "SELECT 'Database summary - tracked entity instances per program' AS D2_DOCKER_PRESQL_SCRIPT;\n")
     write(f, """
 SELECT
     p.uid  AS program_uid,
